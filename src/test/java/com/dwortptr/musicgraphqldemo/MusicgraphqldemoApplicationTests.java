98fc20f52f1a9b4ed43e2a8a79a49e3ce8b77601
Bryan Tidwell <bryan@Bryans-MBP.attlocal.net>
2024-06-02 16:47:39 -0500

    initial commit

---
+++ src/test/java/com/dwortptr/musicgraphqldemo/MusicgraphqldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwortptr.musicgraphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicgraphqldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
